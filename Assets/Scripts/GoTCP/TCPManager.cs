7ded2a4c3dcd9625e359a0bda22bc67127962587
perish95 <perish95k@naver.com>
2024-10-21 11:03:06 +0900

    Fix:다른 플레이어 위치 동기화

--- Assets/Scripts/GoTCP/TCPManager.cs
+++ Assets/Scripts/GoTCP/TCPManager.cs
@@ -33,8 +33,8 @@ public class TCPManager : MonoBehaviour
         if (!hasExecuted)
         {
             playerId = Random.Range(0, 1000).ToString();
-            GameManager.Instance.RegisterPlayer(playerId);
-            TcpProtobufClient.Instance.SendLoginMessage(playerId);
+            //TcpProtobufClient.Instance.SendLoginMessage(playerId);
+            //GameManager.Instance.RegisterPlayer(playerId);
             hasExecuted = true;
         }
     }
